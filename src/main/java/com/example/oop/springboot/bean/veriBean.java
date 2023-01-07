69be424796602f4d45959834e5acc99164343319
mrtark <murat_ark97@hotmail.com>
2023-01-07 15:55:09 +0300

    Bean Yapısı

---
+++ src/main/java/com/example/oop/springboot/bean/veriBean.java
@@ -0,0 +1,18 @@
+package com.example.oop.springboot.bean;
+
+import com.example.oop.springboot.business.dto.ProductDTO;
+
+public class veriBean {
+    public ProductDTO productDTO(){
+        return ProductDTO
+                .builder()
+                .id(1L)
+                .urunAdı("Computer")
+                .urunFiat("35.000")
+                .build();
+    }
+    public static void main(String[] args) {
+        veriBean reVeriBean = new veriBean();
+        System.out.println(reVeriBean.productDTO());
+    }
+}
---
+++ src/main/java/com/example/oop/springboot/business/dto/ProductDTO.java
@@ -0,0 +1,18 @@
+package com.example.oop.springboot.business.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Log4j2
+public class ProductDTO {
+    private Long id;
+    private String urunAdı;
+    private String urunFiat;
+}
