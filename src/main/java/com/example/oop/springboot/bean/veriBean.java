a4d452ed8102668550dbbe314d3bed5816c18d42
mrtark <murat_ark97@hotmail.com>
2023-01-07 15:57:16 +0300

    Bean Yapısı için Lombok Kütüphanesi

--- src/main/java/com/example/oop/springboot/bean/veriBean.java
+++ src/main/java/com/example/oop/springboot/bean/veriBean.java
@@ -1,8 +1,12 @@
 package com.example.oop.springboot.bean;
 
 import com.example.oop.springboot.business.dto.ProductDTO;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class veriBean {
+    @Bean
     public ProductDTO productDTO(){
         return ProductDTO
                 .builder()
